5e5cec0263a8417c9b1ebe4e30ba9fdcec20238d
kotteshiro <kotteshiro@gmail.com>
2014-08-29 16:29:26 -0400

    generando base

---
+++ plantilla/cfg.js
@@ -0,0 +1,8 @@
+var cfg={
+	title: "titulito",
+	scenes:[], //la primera es la principal(inicial)
+	assets:{ //Generales, que no son exclusivos de la escena pe: puntero, sonido evento, fondo, etc.
+		sound:[],
+		images:[]
+	}
+}
